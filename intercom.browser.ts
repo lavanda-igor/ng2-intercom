ca03395bb7d962e852d615cdfb5d05a888bf2516
Chris Moyer <cmoyer@newstex.com>
2017-01-09 11:56:02 -0500

    Normalize intercom initialization for TypeScript

--- intercom.browser.ts
+++ intercom.browser.ts
@@ -1,10 +1,29 @@
-
+/**
+ * Intercom Browser module for Angular
+ */
 export class IntercomBrowser {
+	app_id: string;
+
 	init (data) {
+		if (data.app_id) {
+			this.app_id = data.app_id;
+		}
+		let w = (<any> window);
 		// Intercom initialization
-		window.intercomSettings = data;
-		if (window.Intercom === undefined){
-			(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src=`https://widget.intercom.io/widget/${data.app_id}`;var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+		w.intercomSettings = data;
+		if (w.Intercom === undefined){
+			let i = function (){
+				i.c(arguments);
+			};
+			i.q=[]; i.c= function(args){
+				i.q.push(args)
+			};
+			w.Intercom = i;
+			if(w.attachEvent){
+				w.attachEvent('onload',this.loader);
+			} else {
+				w.addEventListener('load',this.loader,false);
+			}
 		}
 		(<any> window).Intercom("boot", data);
 	}
@@ -16,5 +35,14 @@ export class IntercomBrowser {
 	}
 	shutdown () {
 		(<any> window).Intercom("shutdown");
+	}
+
+	loader () {
+		let s = document.createElement('script');
+		s.type='text/javascript';
+		s.async=true;
+		s.src=`https://widget.intercom.io/widget/${this.app_id}`;
+		let x = document.getElementsByTagName('script')[0];
+		x.parentNode.insertBefore(s,x);
 	}
 }
